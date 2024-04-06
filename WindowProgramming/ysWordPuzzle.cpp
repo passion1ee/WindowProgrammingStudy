8220626501c0a34a52ab722b41b50747be91d34f
이윤상 <64140710+passion1ee@users.noreply.github.com>
2024-04-06 10:36:09 +0900

    test1

---
+++ WindowProgramming/ysWordPuzzle.cpp
@@ -0,0 +1,298 @@
+#include "ysWordPuzzle.h"
+#include "stdfax.h"
+
+namespace ys
+{
+	BYTE WordPuzzle::mapSize;
+	double WordPuzzle::xGrid;
+	double WordPuzzle::yGrid;
+
+	POINT WordPuzzle::player;
+	std::vector<WordPuzzle::Object> WordPuzzle::alphabets;
+	std::vector<std::vector<wchar_t>> WordPuzzle::plain;
+	std::wstring WordPuzzle::goalWord;
+
+	void WordPuzzle::setScreen(int width, int height)
+	{
+		screenWidth = width;
+		screenHeight = height;
+		xGrid = screenWidth / static_cast<double>(mapSize);
+		yGrid = screenHeight / static_cast<double>(mapSize);
+	}
+
+	void WordPuzzle::Init()
+	{
+		mapSize = 20;
+		std::vector<std::wstring> allWord = { L"APPLE", L"DEBUG", L"GAME", L"WINDOWS", L"HETEROGENEOUS" };
+		std::uniform_int_distribution<> selectWord(0, allWord.size() - 1);
+		goalWord = allWord[selectWord(randomEngine)];
+
+		int allWordSize{};
+		for (auto& word : allWord)
+			allWordSize += word.size();
+		alphabets.reserve(26 + allWordSize);
+		plain.assign(mapSize, std::vector<wchar_t>(mapSize, L''));
+
+		std::uniform_int_distribution<> position(0, mapSize - 1);
+		for (auto i = L'A'; i <= L'Z';)
+		{
+			auto x = position(randomEngine); auto y = position(randomEngine);
+			if (plain[x][y] != L'' || y == 0 || y == 1)
+				continue;
+
+			plain[x][y] = i;
+			alphabets.push_back(Object(i, POINT(x, y), 0));
+			++i;
+		}
+
+		for (auto& word : allWord)
+		{
+			if (word == goalWord)
+				continue;
+			for (auto i = 0; i < word.size();)
+			{
+				auto x = position(randomEngine); auto y = position(randomEngine);
+				if (plain[x][y] != L'' || y == 0 || y == 1)
+					continue;
+
+				plain[x][y] = word[i];
+				alphabets.push_back(Object(word[i], POINT(x, y), 0));
+				++i;
+			}
+		}
+
+		auto wordStart = (mapSize - (goalWord.size())) / 2;
+		auto tmpWord = goalWord;
+		std::uniform_int_distribution<> goalPosition(wordStart, wordStart + goalWord.size());
+		for (auto& piece : goalWord)
+		{
+			plain[wordStart++][1] = piece;
+			alphabets.push_back(Object(i, POINT(x, y), 0));
+			++i;
+		}
+	}
+
+	void WordPuzzle::Run(HWND hWnd)
+	{
+		InputManager::BeforeUpdate();
+		Update(hWnd);
+		InputManager::AfterUpdate();
+	}
+
+	void WordPuzzle::Update(HWND hWnd)
+	{
+
+		if (InputManager::getKeyDown(VK_LEFT))
+		{
+			if (player.x == 0)
+				player.x = mapSize - 1;
+			else
+				player.x--;
+			auto iter = std::find_if(alphabets.begin(), alphabets.end(), [](const Object& other) {
+				return player.x == other.position.x && player.y == other.position.y; });
+			if (alphabets.end() != iter)//플레이어의 위치와 동일한 블럭이 있는가
+			{
+				if (iter->position.x == 0)
+					iter->position.x = mapSize - 1;
+				else
+					iter->position.x--;
+				auto otherIter = std::find_if(alphabets.begin(), alphabets.end(), [iter](const Object& other) {
+					return iter->position.x == other.position.x &&
+						iter->position.y == other.position.y &&
+						other != *iter; });
+				if (alphabets.end() != otherIter)//블럭의 이동 후 위치와 동일한 블럭이 있는가
+				{
+					if (player.x == mapSize - 1)
+						player.x = 0;
+					else
+						player.x++;
+					if (iter->position.x == mapSize - 1)
+						iter->position.x = 0;
+					else
+						iter->position.x++;
+				}
+				else
+				{
+					Beep(1760, 40);
+					Beep(2218, 80);
+				}
+			}
+		}
+		if (InputManager::getKeyDown(VK_RIGHT))
+		{
+			if (player.x == mapSize - 1)
+				player.x = 0;
+			else
+				player.x++;
+			auto iter = std::find_if(alphabets.begin(), alphabets.end(), [](const Object& other) {
+				return player.x == other.position.x && player.y == other.position.y; });
+			if (alphabets.end() != iter)//플레이어의 위치와 동일한 블럭이 있는가
+			{
+				if (iter->position.x == mapSize - 1)
+					iter->position.x = 0;
+				else
+					iter->position.x++;
+				auto otherIter = std::find_if(alphabets.begin(), alphabets.end(), [iter](const Object& other) {
+					return iter->position.x == other.position.x &&
+						iter->position.y == other.position.y &&
+						other != *iter; });
+				if (alphabets.end() != otherIter)//블럭의 이동 후 위치와 동일한 블럭이 있는가
+				{
+					if (player.x == 0)
+						player.x = mapSize - 1;
+					else
+						player.x--;
+					if (iter->position.x == 0)
+						iter->position.x = mapSize - 1;
+					else
+						iter->position.x--;
+				}
+				else
+				{
+					Beep(1760, 40);
+					Beep(2218, 80);
+				}
+			}
+		}
+		if (InputManager::getKeyDown(VK_UP))
+		{
+			if (player.y == 0)
+				player.y = mapSize - 1;
+			else
+				player.y--;
+			auto iter = std::find_if(alphabets.begin(), alphabets.end(), [](const Object& other) {
+				return player.x == other.position.x && player.y == other.position.y; });
+			if (alphabets.end() != iter)//플레이어의 위치와 동일한 블럭이 있는가
+			{
+				if (iter->position.y == 0)
+					iter->position.y = mapSize - 1;
+				else
+					iter->position.y--;
+				auto otherIter = std::find_if(alphabets.begin(), alphabets.end(), [iter](const Object& other) {
+					return iter->position.x == other.position.x &&
+						iter->position.y == other.position.y &&
+						other != *iter; });
+				if (alphabets.end() != otherIter)//블럭의 이동 후 위치와 동일한 블럭이 있는가
+				{
+					if (player.y == mapSize - 1)
+						player.y = 0;
+					else
+						player.y++;
+					if (iter->position.y == mapSize - 1)
+						iter->position.y = 0;
+					else
+						iter->position.y++;
+				}
+				else
+				{
+					Beep(1760, 40);
+					Beep(2218, 80);
+				}
+			}
+		}
+		if (InputManager::getKeyDown(VK_DOWN))
+		{
+			if (player.y == mapSize - 1)
+				player.y = 0;
+			else
+				player.y++;
+			auto iter = std::find_if(alphabets.begin(), alphabets.end(), [](const Object& other) {
+				return player.x == other.position.x && player.y == other.position.y; });
+			if (alphabets.end() != iter)//플레이어의 위치와 동일한 블럭이 있는가
+			{
+				if (iter->position.y == mapSize - 1)
+					iter->position.y = 0;
+				else
+					iter->position.y++;
+				auto otherIter = std::find_if(alphabets.begin(), alphabets.end(), [iter](const Object& other) {
+					return iter->position.x == other.position.x &&
+						iter->position.y == other.position.y &&
+						other != *iter; });
+				if (alphabets.end() != otherIter)//블럭의 이동 후 위치와 동일한 블럭이 있는가
+				{
+					if (player.y == 0)
+						player.y = mapSize - 1;
+					else
+						player.y--;
+					if (iter->position.y == 0)
+						iter->position.y = mapSize - 1;
+					else
+						iter->position.y--;
+				}
+				else
+				{
+					Beep(1760, 40);
+					Beep(2218, 80);
+				}
+			}
+		}
+
+		if (InputManager::getKeyDown((UINT)Key::S))
+		{
+			Init();
+		}
+		if (InputManager::getKeyUp((UINT)Key::Q))
+		{
+			PostMessage(hWnd, WM_CLOSE, 0, 0);
+		}
+
+		if (isWord())
+		{
+			victorySound();
+			MessageBox(hWnd, L"VICTORY!", L"", NULL);
+			Init();
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
+	}
+
+	void WordPuzzle::renderFrame(HDC hDC)
+	{
+		for (int i = 0; i < mapSize; ++i)
+		{
+			MoveToEx(hDC, 0, i * yGrid, NULL);
+			LineTo(hDC, screenWidth, i * yGrid);
+		}
+		for (int i = 0; i < mapSize; ++i)
+		{
+			MoveToEx(hDC, i * xGrid, 0, NULL);
+			LineTo(hDC, i * xGrid, screenHeight);
+		}
+	}
+	bool WordPuzzle::isWord()
+	{
+		for (auto& alphabet : alphabets)
+		{
+
+		}
+	}
+	void WordPuzzle::victorySound()
+	{
+		Beep(130, 100);
+		Beep(262, 100);
+		Beep(330, 100);
+		Beep(392, 100);
+		Beep(523, 100);
+		Beep(660, 100);
+		Beep(784, 300);
+		Beep(660, 300);
+		Beep(146, 100);
+		Beep(262, 100);
+		Beep(311, 100);
+		Beep(415, 100);
+		Beep(523, 100);
+		Beep(622, 100);
+		Beep(831, 300);
+		Beep(622, 300);
+		Beep(155, 100);
+		Beep(294, 100);
+		Beep(349, 100);
+		Beep(466, 100);
+		Beep(588, 100);
+		Beep(699, 100);
+		Beep(933, 300);
+		Beep(933, 100);
+		Beep(933, 100);
+		Beep(933, 100);
+		Beep(1047, 400);
+	}
+}
---
+++ WindowProgramming/ysWordPuzzle.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <windows.h>
+#include <vector>
+#include "../../WinProgramming/MyEngine_source/ysInputManager.h"
+
+namespace ys
+{
+
+	class WordPuzzle
+	{
+	public:
+		struct Object
+		{
+			int id;
+			wchar_t alphabet;
+			POINT position;
+
+			Object() = delete;
+			Object(wchar_t alphabet_, POINT _position, int _id)
+				: alphabet(alphabet_), position(_position), id(_id) {}
+
+			bool operator==(const Object& other)
+			{
+				if (id != other.id)
+					return false;
+				if (alphabet != other.alphabet)
+					return false;
+				if (position.x != other.position.x || position.y != other.position.y)
+					return false;
+				return true;
+			}
+		};
+
+	public:
+		static void setScreen(int width, int height);
+
+		static void Init();
+		static void Run(HWND hWnd);
+		static void render(HDC hDC);
+		static void select(WPARAM w);
+
+	private:
+		static void Update(HWND hWnd);
+		static void renderFrame(HDC hDC);
+		static void renderObject(HDC hDC, Object object);
+
+		static bool isWord();
+		static void victorySound();
+	private:
+		static int screenWidth;
+		static int screenHeight;
+
+		static BYTE mapSize;
+		static double xGrid;
+		static double yGrid;
+
+		static POINT player;
+		static std::vector<Object> alphabets;
+		static std::vector<std::vector<wchar_t>> plain;
+		static std::wstring goalWord;
+	};
+}
+
