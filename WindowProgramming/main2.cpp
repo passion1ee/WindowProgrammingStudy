611b34bb5e5daccf127cd9f736fb75e809414f2f
이윤상 <64140710+passion1ee@users.noreply.github.com>
2024-04-09 22:37:09 +0900

    file refactoring

---
+++ WindowProgramming/main2.cpp
@@ -0,0 +1,26 @@
+#include <Windows.h>
+#include <tchar.h>
+#include <iostream>
+
+#define PRACTICE__3
+#define PRACTICE__3_1
+
+
+#ifdef PRACTICE__3
+#ifdef PRACTICE__3_1
+#pragma comment(linker,"/entry:WinMainCRTStartup /subsystem:console")
+#include <windowsx.h>
+#include "ysDigitAlphaPlus.h"
+
+#include "..\\..\\WinProgramming\\MyEngine_source\\ysInputManager.h"
+#include "..\\..\\WinProgramming\\MyEngine_source\\ysTimer.h"
+#pragma comment (lib, "..\\..\\WinProgramming\\x64\\Debug\\MyEngine.lib")
+
+
+
+
+#endif // PRACTICE__3_1
+
+#endif // PRACTICE__3
+
+//�ѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤѤ�
